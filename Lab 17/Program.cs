4dd7d0e105ceed1c32c8f8eb77601edd9bbb9a7f
VICTORIA TRINOSOVA <trinosova.victoria@yandex.ru>
2021-12-06 18:18:55 +0300

    1

---
+++ Lab 17/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace Lab_17
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Bankaccount<string> bankaccount1 = new Bankaccount<string>("01", 122222,"Иванов Иван Иванович");
+            Console.WriteLine("Номер счета: {0}",bankaccount1.Number);
+            Console.WriteLine("Баланс: {0}",bankaccount1.Balance);
+            Console.WriteLine("ФИО владельца: {0}",bankaccount1.Name);
+
+            Bankaccount<int> bankaccount2 = new Bankaccount<int>(1, 122222, "Иванов Иван Иванович");
+            Console.WriteLine("Номер счета: {0}", bankaccount2.Number);
+            Console.WriteLine("Баланс: {0}", bankaccount2.Balance);
+            Console.WriteLine("ФИО владельца: {0}", bankaccount2.Name);
+
+        }
+    }
+    class Bankaccount <T>
+    {
+        public T Number { get; set; }
+        public int Balance { get; set; }
+        public string Name { get; set; }
+        public Bankaccount (T number,int balance, string name)
+        {
+            this.Number = number;
+            this.Balance = balance;
+            this.Name = name;
+        }
+    }
+}
